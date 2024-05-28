6837bea6662fd40c36b0afd855b4d9faef476938
Carlo Ettisberger <carlo_ettisberger@hotmail.com>
2024-05-28 14:22:44 +0200

    changes

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+import ThreeCanvas from './three.js';
+
+function App() {
+  return (
+    <div className="App">
+      <section className="canvas" id="canvas">
+        <ThreeCanvas />
+      </section>
+    </div>
+  );
+}
+
+export default App;
