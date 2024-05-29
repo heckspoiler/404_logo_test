047594ec954d304fb22bbc885b153b3db3cb375c
Carlo Ettisberger <carlo_ettisberger@hotmail.com>
2024-05-29 10:46:26 +0200

    added react again

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
+import logo from './logo.svg';
 import './App.css';
-
-import ThreeCanvas from './three.js';
 
 function App() {
   return (
     <div className="App">
-      <section className="canvas" id="canvas">
-        <ThreeCanvas />
-      </section>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
